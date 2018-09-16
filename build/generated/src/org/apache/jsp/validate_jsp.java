e715aed91bd19fc725b21c8ffa460e1a4038ee17
Ahad Patrawala <41312423+ahadpatrawala@users.noreply.github.com>
2018-09-16 15:21:58 -0700

    Add files via upload

---
+++ build/generated/src/org/apache/jsp/validate_jsp.java
@@ -0,0 +1,87 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import java.sql.*;;
+
+public final class validate_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("        ");
+
+        String user_id=request.getParameter("t1");
+        String password=request.getParameter("t2");
+        try
+        {
+            Class.forName("org.gjt.mm.mysql.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/timetable","root","");
+            PreparedStatement ps=con.prepareStatement("select * from information where user_id=? and password=?");
+            ps.setString(1,user_id);
+            ps.setString(2,password);
+            ResultSet rs=ps.executeQuery();
+            if(rs.next())
+            {
+                session.setAttribute("user_id",user_id);
+                session.setAttribute("password", password);
+                response.sendRedirect("teacherhome.java");
+            }
+            else
+            {
+                out.println("Login Unsuccessful");
+            }
+        }
+        catch(Exception e)
+        {
+            out.println("Error: "+e);
+        }
+
+      out.write('\n');
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
